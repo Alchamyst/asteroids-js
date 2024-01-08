6637222e2ff8eb3f331ac5cf03e1a7f939bbf0f1
alchamyst <ashley@ashley-yetman.co.uk>
2024-01-08 20:29:21 +0000

    add inputManager to handle user inputs. Add functionality for ship movement.

--- asteroids.js
+++ asteroids.js
@@ -9,6 +9,8 @@ let oldTimeStamp = 0;
 let fps;
 let hud;
 
+let gameManager;
+let inputManager;
 let gameObjects = [];
 
 document.addEventListener('DOMContentLoaded', init); 
@@ -20,13 +22,27 @@ function init(){
     canvas.height = canvasHeight;
     clearScreen();
 
+    gameManager = new GameManager;
+    inputManager = new InputManager;
+    document.body.addEventListener("keydown", function(e){inputManager.KeyDown(e.keyCode)});
+    document.body.addEventListener("keyup", function(e){inputManager.KeyUp(e.keyCode)});
 
     // livesCounter = new LivesCounter(context);
     // timersHUD = new TimersHUD(context);
-
+    
     gameObjects.push(new TimersHUD(context));
     gameObjects.push(new LivesCounter(context));
     gameObjects.push(new Ship(context));
+
+    // document.body.addEventListener("keydown", function(e){
+    //     keys[e.keyCode] = true;
+    // });
+    // document.body.addEventListener("keyup", function(e){
+    //     keys[e.keyCode] = false;
+    //     if(e.keyCode === 32 && ship.visible){
+    //         bullets.push(new Bullet(ship.angle));
+    //     }
+    // });
     // let Player = new Ship(context, canvasWidth / 2, canvasHeight / 2, 0, 0, collisionRadius);
 
     window.requestAnimationFrame(gameLoop);
@@ -45,25 +61,14 @@ function gameLoop(timeStamp) {
     // Calculate fps
     fps = Math.round(1 / secondsPassed);
 
-    // Draw number to the screen
-    // context.fillStyle = 'white';
-    // context.fillRect(0, 0, 200, 100);
-    // context.font = '25px Arial';
-    // context.fillStyle = 'black';
-    // context.fillText("FPS: " + fps, 10, 30);
-
     clearScreen();
-    // livesCounter.Render();
-    // timersHUD.Render();
 
-    // Loop over all game objects
-    // for (let i = 0; i < gameObjects.length; i++) {
-    //     gameObjects[i].update(secondsPassed);
-    // }
+    // Loop over all game objects and update.
+    for (let i = 0; i < gameObjects.length; i++) {
+        gameObjects[i].Update(secondsPassed);
+    }
 
-   
-
-    // Do the same to draw
+    // Loop over all game objects and draw.
     for (let i = 0; i < gameObjects.length; i++) {
         gameObjects[i].Render();
     }
@@ -91,13 +96,63 @@ class GameManager {
     }
 }
 
+class InputManager {
+    constructor(){
+        this.actions = {
+            leftButton: false,
+            rightButton: false,
+            forwardButton: false,
+            fireButton: false,
+            startButton: false
+        }
+    }
+    KeyDown(keyCode){
+        // UpArrow or W key pressed.
+        console.log(`keyCode=${keyCode}`)
+        if(keyCode === 38 || keyCode == 87){this.actions.forwardButton = true; console.log("forwardButton down")}
+        
 
+        // LeftArrow or A key pressed.
+        if(keyCode === 37 || keyCode == 65){this.actions.leftButton = true}
+
+        // RightArrow or D key pressed.
+        if(keyCode === 39 || keyCode == 68){this.actions.rightButton = true}
+
+        // Spacebar key pressed.
+        if(keyCode === 32){this.actions.fireButton = true}
+
+        // Enter key pressed.
+        if(keyCode === 13){this.actions.startButton = true}
+    }
+    KeyUp(keyCode){
+        // UpArrow or W key pressed.
+        if(keyCode === 38 || keyCode === 87){this.actions.forwardButton = false; console.log("forwardButton up")}
+
+        // LeftArrow or A key pressed.
+        if(keyCode === 37 || keyCode === 65){this.actions.leftButton = false}
+
+        // RightArrow or D key pressed.
+        if(keyCode === 39 || keyCode === 68){this.actions.rightButton = false}
+
+        // Spacebar key pressed.
+        if(keyCode === 32){this.actions.fireButton = false}
+
+        // Enter key pressed.
+        if(keyCode === 13){this.actions.startButton = false}
+    }
+}
 
 class GameObject {
     constructor(ctx, x, y){
         this.ctx = ctx;
         this.x = x;
         this.y = y;
+    }
+    Update(){
+
+    }
+    Render(){
+
     }
 }
 
@@ -155,20 +210,52 @@ class PhysicsObject extends GameObject {
     }
 }
 
-class Ship extends GameObject {
+class Ship extends PhysicsObject {
     constructor(ctx){
-        super(ctx, canvasWidth / 2 , canvasHeight / 2);
-        this.speed = 0.075;
-        this.rotateSpeed = 0.00075;
+        super(ctx, canvasWidth / 2 , canvasHeight / 2, 0, 0, 11);
+        this.speed = 10;
+        this.rotateSpeed = 0.075;
         this.radius = 15;
-        this.collisionRadius = 11;
+        // this.collisionRadius = 11;
+        this.dirModifier = 0;
         this.angle = 0;
         this.strokeColor = 'white';
         this.noseX = canvasWidth / 2 + 15;
         this.noseY = canvasHeight / 2;
+        this.movingForward = false;
     }
-    Update(){
+    Update(secondsPassed){
+        // Check for inputs affecting ship actions.
+        this.movingForward = inputManager.actions.forwardButton;
+        this.dirModifier = 0;
+        if(inputManager.actions.leftButton) this.dirModifier = -1;
+        if(inputManager.actions.rightButton) this.dirModifier = 1;
+        if(inputManager.actions.fireButton){
+            //fire bullets
+            console.log("Pew pew");
+        };
 
+        let radians = this.angle / Math.PI * 180; //convert from degrees to radians.
+
+        // Increase velocity if movingForward.
+        if(this.movingForward) {
+            this.velocityX += Math.cos(radians) * this.speed;
+            this.velocityY += Math.sin(radians) * this.speed;
+        }
+
+        // Move the ship to the other side of the screen if we move out of bounds.
+        if(this.x < this.radius) this.x = canvas.width; 
+        if(this.x > canvas.width) this.x = this.radius; 
+        if(this.y < this.radius) this.y = canvas.height; 
+        if(this.y > canvas.height) this.y = this.radius; 
+
+        // Reduce velocity. Imposes speed cap.
+        this.velocityX *= 0.99;
+        this.velocityY *= 0.99;
+
+        this.angle += (this.rotateSpeed * this.dirModifier) * secondsPassed;
+        this.x -= (this.velocityX * secondsPassed);
+        this.y -= (this.velocityY * secondsPassed);
     }
     Render(){
         this.ctx.strokeStyle = this.strokeColor;
