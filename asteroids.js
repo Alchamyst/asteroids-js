fff3cde91a3355d9a2edd23f216bb010c4d5831b
alchamyst <ashley@ashley-yetman.co.uk>
2024-01-09 00:41:36 +0000

    expandGameManager functionality. add Bullets.

--- asteroids.js
+++ asteroids.js
@@ -1,4 +1,3 @@
-
 const canvasWidth = 1200;
 const canvasHeight = 800;
 let canvas;
@@ -24,19 +23,10 @@ function init(){
 
     gameManager = new GameManager;
     inputManager = new InputManager;
-    document.body.addEventListener("keydown", function(e){inputManager.KeyDown(e.keyCode)});
-    document.body.addEventListener("keyup", function(e){inputManager.KeyUp(e.keyCode)});
+    document.body.addEventListener("keydown", (e) => {inputManager.KeyDown(e.keyCode)});
+    document.body.addEventListener("keyup", (e) => {inputManager.KeyUp(e.keyCode)});
 
-    // livesCounter = new LivesCounter(context);
-    // timersHUD = new TimersHUD(context);
-    
-    gameObjects.push(new TimersHUD());
-    gameObjects.push(new LivesCounter());
-    gameObjects.push(new Ship());
-
-    for(let i = 0; i < 7; i++){
-        gameObjects.push(new Asteroid());
-    }
+    gameManager.Init();
 
     window.requestAnimationFrame(gameLoop);
 }
@@ -62,6 +52,7 @@ function gameLoop(timeStamp) {
     }
 
     detectCollisions();
+    gameManager.CheckGameState();
 
     // Loop over all game objects and draw.
     for (let i = 0; i < gameObjects.length; i++) {
@@ -83,44 +74,31 @@ function detectCollisions(){
     }
 
     // Check Physics objects for Collisions
-    for (let i=0; i < gameObjects.length; i++){
+    for (let i = gameObjects.length -1; i >= 0; i--){
         obj1 = gameObjects[i];
 
         if (obj1 instanceof PhysicsObject){
-            for (let j=0; j < gameObjects.length; j++){
+            for (let j = gameObjects.length - 1; j >= 0; j--){
                 obj2 = gameObjects[j];
                 
                 if (obj2 instanceof PhysicsObject && i !==j){
-
-                    // Compare object1 with object2
-                    // if(CircleCollision(obj1.x, obj1.y, obj1.collisionRadius, obj2.x, obj2.y, obj2.collisionRadius)){
-                    //     console.log(`Collision detcted between ${obj1} and ${obj2}`);
-                    //     obj1.isColliding = true;
-                    //     obj1.isColliding = true;
-                    // }
                     if(circleIntersect(obj1.x, obj1.y, obj1.collisionRadius, obj2.x, obj2.y, obj2.collisionRadius)){
-                        console.log(`Collision detcted between ${obj1} and ${obj2}`);
                         obj1.isColliding = true;
                         obj1.isColliding = true;
+                            
+                        if ((obj1 instanceof Bullet || obj2 instanceof Bullet) && (obj1 instanceof Asteroid || obj2 instanceof Asteroid)){
+                            if(obj1 instanceof Asteroid){
+                                obj1.handleBullethit
+                            }
+                            console.log(`Asteroid was shot!`);
+                            // the asteroid and bullet must both be destroyed
+                        }
                     }
                 }
             }
         }
     }
 }
-
-// function CircleCollision(p1x, p1y, r1, p2x, p2y, r2){
-//     let radiusSum;
-//     let xDiff;
-//     let yDiff;
-//     radiusSum = r1 + r2;
-//     xDiff = p1x - p2x;
-//     yDiff = p1y - p2y;
-//     if(radiusSum > Math.sqrt((xDiff * xDiff) + (yDiff * yDiff))){
-//         return true;
-//     }
-//     return false;
-// }
 
 function circleIntersect(x1, y1, r1, x2, y2, r2){
     // Calculate the distance between the two circles
@@ -130,23 +108,72 @@ function circleIntersect(x1, y1, r1, x2, y2, r2){
     return squareDistance <= ((r1 + r2) * (r1 + r2))
 }
 
+function removeInstances(itemType){
+    for (let i = gameObjects.length - 1; i >= 0; i--) {
+        if (gameObjects[i] instanceof itemType) {
+            gameObjects.splice(i, 1);
+        }
+    }   
+}
 
 class GameManager {
     constructor(){
         this.gameState = 'START'; // START, RUNNING, PAUSE, GAME_OVER
-        this.currentLives = 3;
+        this.currentLives = 0;
         this.currentScore = 0;
+        this.asteroidCount = 8;
+    }
+    Init(){
+        // Create HUD & Some Asteroids
+        // gameObjects.push(new TimersHUD());
+        gameObjects.push(new LivesCounter());
+        gameObjects.push(new GameMsg('Asteroids','Press Enter To Start'));
+        this.SpawnAsteroids(10);
+    }
+    CheckGameState(){
+        switch(this.gameState) {
+            case 'START':
+                if(inputManager.actions.startButton){
+                    removeInstances(GameMsg);
+                    this.NewGame();
+                } 
+                break; 
+
+            case 'RUNNING':
+                break;
+
+            case 'GAME_OVER':
+                if(inputManager.actions.startButton){
+                    removeInstances(GameMsg);
+                    this.NewGame();
+                } 
+                break;
+
+            default:
+                console.log("GameState Error.")
+        }
     }
     NewGame(){
         this.gameState = 'RUNNING';
         this.currentLives = 3;
         this.currentScore = 0;
-    }
-    PauseGame(){
-        this.gameState = 'PAUSED';
+        removeInstances(Asteroid);
+        this.SpawnAsteroids(this.asteroidCount);
+        gameObjects.push(new Ship());
     }
     GameOver(){
         this.gameState = 'GAME_OVER';
+        for(let i=0; i < gameObjects.length; i++) {
+            if(gameObjects[i] instanceof Ship){
+                gameObjects.splice(i,1);
+            }
+        }
+        gameObjects.push(new GameMsg('GAME OVER','Press Enter To Try Again'));
+    }
+    SpawnAsteroids(amount){
+        for(let i = 0; i < amount; i++){
+            gameObjects.push(new Asteroid());
+        }
     }
 }
 
@@ -214,6 +241,27 @@ class HudElement extends GameObject {
     }
 }
 
+class GameMsg extends HudElement {
+    constructor(text1, text2){
+        super();
+        this.font1 = '50px Arial';
+        this.font2 = '20px Arial';
+        this.text1 = text1 || 'Foo';
+        this.text2 = text2 || 'Bar';
+        this.shouldClose = false;
+    }
+    Update(){
+
+    }
+    Render(){
+        context.fillStyle = this.color;
+        context.font = this.font1;
+        context.fillText(this.text1, (canvasWidth - context.measureText(this.text1).width) /2, canvasHeight / 2);
+        context.font = this.font2;
+        context.fillText(this.text2, (canvasWidth - (context.measureText(this.text2).width)) / 2, (canvasHeight / 2) + 40);
+    }
+}
+
 class LivesCounter extends HudElement {
     constructor(){
         super(1175, 10);     
@@ -242,11 +290,12 @@ class LivesCounter extends HudElement {
 
 class TimersHUD extends HudElement {
     constructor(){
-        super(canvasWidth-125, canvasHeight-25);     
+        super(canvasWidth-125, canvasHeight-25); 
+        this.font = '25px Arial';  
     }
     Render(){
-        context.font = '25px Arial';
-        context.fillStyle = 'white';
+        context.font = this.font;
+        context.fillStyle = this.color;
         context.fillText("FPS: " + fps, this.x, this.y);
         // context.fillText("SP: " + secondsPassed, this.x-50, this.y-40);      
     }
@@ -258,8 +307,10 @@ class PhysicsObject extends GameObject {
         this.velocityX = velocityX;
         this.velocityY = velocityY;
         this.collisionRadius = collisionRadius;
-        this.renderCollision = true; // Useful for debugging.
+        this.renderCollision = false; // Useful for debugging.
         this.isColliding = false; 
+    }
+    Update(){
     }
     Render(){
         if(this.renderCollision){
@@ -276,7 +327,7 @@ class Ship extends PhysicsObject {
     constructor(){
         super(canvasWidth / 2 , canvasHeight / 2, 0, 0, 11);
         this.speed = 10;
-        this.rotateSpeed = 0.075;
+        this.rotateSpeed = 0.1;
         this.radius = 15;
         // this.collisionRadius = 11;
         this.dirModifier = 0;
@@ -285,16 +336,42 @@ class Ship extends PhysicsObject {
         this.noseX = canvasWidth / 2 + 15;
         this.noseY = canvasHeight / 2;
         this.movingForward = false;
+        this.shieldTimer = 3;
+
+        this.bulletTimer = 0;
+        this.bulletFireRate = 0.25;
     }
     Update(secondsPassed){
+        super.Update();
+
+        if(this.isColliding == true && this.shieldTimer == 0){
+            gameManager.currentLives -= 1;
+
+            if(gameManager.currentLives == 0) return gameManager.GameOver();
+
+            this.x = canvasWidth / 2;
+            this.y = canvasHeight / 2;
+            this.velocityX = 0;
+            this.velocityY = 0;
+            this.movingForward = false;
+            this.dirModifier = 0;
+            this.shieldTimer = 3;
+            return
+        }
+
+        this.bulletTimer += secondsPassed;
+
         // Check for inputs affecting ship actions.
         this.movingForward = inputManager.actions.forwardButton;
         this.dirModifier = 0;
         if(inputManager.actions.leftButton) this.dirModifier = -1;
         if(inputManager.actions.rightButton) this.dirModifier = 1;
-        if(inputManager.actions.fireButton){
+        if(inputManager.actions.fireButton && this.bulletTimer >= this.bulletFireRate){
             //fire bullets
-            console.log("Pew pew");
+            console.log("Pew pew!");
+            // console.log(`nose loc: ${this.noseX},${this.noseY}`);
+            gameObjects.push(new Bullet(this.noseX, this.noseY, this.angle));
+            this.bulletTimer = 0;
         };
 
         let radians = this.angle / Math.PI * 180; //convert from degrees to radians.
@@ -312,12 +389,17 @@ class Ship extends PhysicsObject {
         if(this.y > canvas.height) this.y = this.radius; 
 
         // Reduce velocity. Imposes speed cap.
-        this.velocityX *= 0.99;
-        this.velocityY *= 0.99;
+        this.velocityX *= Math.pow(0.99, secondsPassed);
+        this.velocityY *= Math.pow(0.99, secondsPassed);
+        // this.velocityX *= 0.99;
+        // this.velocityY *= 0.99;
 
         this.angle += (this.rotateSpeed * this.dirModifier) * secondsPassed;
         this.x -= (this.velocityX * secondsPassed);
         this.y -= (this.velocityY * secondsPassed);
+
+        // Reduce the shield timer.
+        this.shieldTimer = Math.max(0, this.shieldTimer - secondsPassed);
     }
     Render(){
         super.Render();
@@ -340,9 +422,46 @@ class Ship extends PhysicsObject {
         context.arc(this.noseX, this.noseY, 2,0,2* Math.PI);
         context.closePath();
         context.stroke();
+
+        // Shield visual
+        if (this.shieldTimer > 0){
+            let opacity = this.shieldTimer > 1 ? 0.9 : this.shieldTimer;
+            context.beginPath();
+            context.strokeStyle = `rgba(0, 255, 255, ${opacity})`;
+            context.arc(this.x, this.y, (this.collisionRadius*1.75),0,2* Math.PI);
+            context.closePath();
+            context.stroke();
+        }
     }
 }
 
+class Bullet extends PhysicsObject {
+    constructor(x, y, angle){
+        super(x, y);
+
+        this.angle = angle;
+        this.collisionRadius = 3;
+        this.height = 4;
+        this.width = 4;
+        this.speed = 750;
+        this.velocityX = 0;
+        this.velocityY = 0;
+    }
+    Update(){
+        super.Update();
+        // console.log(`Bullet loc: ${this.x}, ${this.y}`);
+
+        var radians = this.angle / Math.PI * 180;
+        this.x -= Math.cos(radians) * this.speed * secondsPassed;
+        this.y -= Math.sin(radians) * this.speed * secondsPassed;
+    }
+    Render(){
+        super.Render();
+
+        context.fillStyle = 'white';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
 
 class Asteroid extends PhysicsObject {
     constructor(startX, startY, level, speed, radius, collisionRadius){
@@ -356,7 +475,11 @@ class Asteroid extends PhysicsObject {
         this.collisionRadius = collisionRadius || 46;
         this.level = level || 1;
     }
+    ScoredHit(){
+        // gameObjects.splice(i,1);
+    }
     Update(){
+        super.Update();
         // this.angle += 0.1 * secondsPassed;
         var radians = this.angle / Math.PI * 180;
 
